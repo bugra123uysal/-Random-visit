7b4b3f749bd5902603ec0a9d5c5e36a03a3b9fc4
Mesut Buğra UYSAL <121828825+bugra123uysal@users.noreply.github.com>
2024-05-05 01:55:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-/* rasgele ülke harf seçme */
+/* rasgele ülke harf seçme (rasgele ülke seç) */
 
 function rstharf(){
 
     var alfabe = "abcçdefghijklmnopqrstuvwxyz";
-    var ülkelerlst=["amerika,almanya,arnavutluk","belçika,brezilya , brundi","","çek cumhuriyeti,çekoslovakya,çin,","danimarka","estonya","fransa,","gana,","hindistan, ","israil,iran,ingiltere,ispanya,","jameika,","kanada,","Liechtenstein","maceristan,","n","o","portekiz, panama,","yeni zelanda","",""]
+    var ülkelerlst=["amerika,almanya,arnavutluk,avusturya, avustralya","belçika,brezilya,brundi,","","çekya,çekoslovakya,çin,","danimarka","estonya","fransa,","gana,","hindistan,hollanda ","israil,iran,ingiltere,ispanya,","jameika,japonya","kanada,kıbrıs,","Liechtenstein","maceristan,","n","o","portekiz, panama,polonya","yeni zelanda,yunanistan,yugoslavya","",""]
    var rsdur=  Math.floor(Math.random()*alfabe.length)
   var mm= document.getElementById("rhar").innerHTML=alfabe[rsdur];
   var hh=document.getElementById("country").innerHTML=ülkelerlst[rsdur];
 
-  document.getElementById("ülk").innerHTML=mm +" "+" harfi ile ilgili ülkeler"+" "+hh ;
+
 }
 
 /* haritadan rasgele seçmek */
